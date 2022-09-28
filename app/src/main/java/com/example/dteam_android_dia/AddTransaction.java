267a30aee33569f5d98b319bc43a6e59f66c2452
Thuancvce150378 <thuancvce150378@fpt.edu.vn>
2022-09-28 15:37:54 +0700

    bo sung add transaction

---
+++ app/src/main/java/com/example/dteam_android_dia/AddTransaction.java
@@ -0,0 +1,14 @@
+package com.example.dteam_android_dia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddTransaction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_transaction);
+    }
+}
