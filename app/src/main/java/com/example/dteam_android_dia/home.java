2026d365d5c46021d90a898744d312bfa8bd500a
MyKT0419 <khuutumy01@gmail.com>
2022-10-12 15:28:02 +0700

    home

--- app/src/main/java/com/example/dteam_android_dia/home.java
+++ app/src/main/java/com/example/dteam_android_dia/home.java
@@ -24,63 +24,48 @@ public class home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-//        ImageButton btn_edit = findViewById(R.id.img_btn_edit);
-//        btn_edit.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                OpenPopup();
-//            }
-//        });
-
-//        Button btntest = findViewById(R.id.btnTest);
-//        btntest.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                OpenPopup();
-//            }
-//        });
     }
 
-    public void OpenPopup(View v){
-        final Dialog dialog = new Dialog(this);
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        dialog.setContentView(R.layout.activity_setup_income);
-
-        Window window = dialog.getWindow();
-        if (window == null) {
-            return;
-        }
-
-        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
-        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-
-        WindowManager.LayoutParams windowAttributes = window.getAttributes();
-        windowAttributes.gravity = Gravity.CENTER;
-        window.setAttributes(windowAttributes);
-
-//        if(Gravity.BOTTOM == Gravity.CENTER){
-//            dialog.setCancelable(true);
-//        }else{
-//            dialog.setCancelable(false);
+//    public void OpenPopup(View v){
+//        final Dialog dialog = new Dialog(this);
+//        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        dialog.setContentView(R.layout.activity_setup_income);
+//
+//        Window window = dialog.getWindow();
+//        if (window == null) {
+//            return;
 //        }
-
-//        Button btnCancel = dialog.findViewById(R.id.btnCancel);
-//        Button btnSend = dialog.findViewById(R.id.btnSend);
 //
-//        btnCancel.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                dialog.dismiss();
-//            }
-//        });
+//        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+//        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 //
-//        btnSend.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Toast.makeText(home.this, "Send", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-        dialog.show();
-    }
+//        WindowManager.LayoutParams windowAttributes = window.getAttributes();
+//        windowAttributes.gravity = Gravity.CENTER;
+//        window.setAttributes(windowAttributes);
+//
+////        if(Gravity.BOTTOM == Gravity.CENTER){
+////            dialog.setCancelable(true);
+////        }else{
+////            dialog.setCancelable(false);
+////        }
+//
+////        Button btnCancel = dialog.findViewById(R.id.btnCancel);
+////        Button btnSend = dialog.findViewById(R.id.btnSend);
+////
+////        btnCancel.setOnClickListener(new View.OnClickListener() {
+////            @Override
+////            public void onClick(View view) {
+////                dialog.dismiss();
+////            }
+////        });
+////
+////        btnSend.setOnClickListener(new View.OnClickListener() {
+////            @Override
+////            public void onClick(View view) {
+////                Toast.makeText(home.this, "Send", Toast.LENGTH_SHORT).show();
+////            }
+////        });
+//        dialog.show();
+//    }
 
 }
