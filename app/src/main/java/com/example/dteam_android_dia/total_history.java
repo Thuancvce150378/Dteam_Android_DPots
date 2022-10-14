0ac027e27fee4fac6343680afffa4a62fe9fd042
thanhtuyenne <88424555+thanhtuyenne@users.noreply.github.com>
2022-10-14 22:48:25 +0700

    page total history

---
+++ app/src/main/java/com/example/dteam_android_dia/total_history.java
@@ -0,0 +1,13 @@
+package com.example.dteam_android_dia;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class total_history extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_total_history);
+    }
+}
